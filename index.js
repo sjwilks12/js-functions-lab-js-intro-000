2b72f72926b54f25c5e8d609e7de883b92c7f817
Samuel J Wilkins <wilkins.samuelj@gmail.com>
2019-10-10 22:31:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function happyHolidays() {
   return "Happy holidays!"
-}// write your code below!
+}
 
 function happyHolidaysTo(name) {
   return "Happy holidays, ${name}!"
@@ -8,4 +8,8 @@ function happyHolidaysTo(name) {
 
 function happyHolidayTo(holiday, name) {
   return "Happy ${holiday}, ${name}!"
+}
+
+function holidayCountdown(holiday, days) {
+  return "It's ${days} days until ${holiday}!"
 }
